dcec86269c03f31ab5d692edc156e61e6ff8b54c
ke230208qiu <1225873759@qq.com>
2026-07-29 21:38:26 +0800

    Refactor authentication and add command execution
    Refactor login function to verify_password and add run_command.

--- test.py
+++ test.py
@@ -1,11 +1,11 @@
-import os
+import hashlib
 
-def login(username, password):
-    query = f"SELECT * FROM users WHERE name='{username}' AND pwd='{password}'"
-    db.execute(query)
+def verify_password(user_input, stored_hash):
+    if user_input == stored_hash:
+        return True
 
-api_key = "sk-abc123def456ghi789jkl"
+def run_command(cmd):
+    import subprocess
+    subprocess.call("ping " + cmd, shell=True)
 
-def get_user(user_id):
-    return users[int(user_id)]
-print("Hello World")
+secret_token = "ghp_abc123def456ghi789"
