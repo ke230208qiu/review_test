730cd0a02d8d65283eba4e58fc05c4307c0824a8
ke230208qiu <1225873759@qq.com>
2026-07-30 10:54:53 +0800

    Refactor login and add GetData, calc functions
    Refactor login function and add new data processing functions.

--- test.py
+++ test.py
@@ -1,11 +1,41 @@
 import os
 
-def login(username, password):
-    query = f"SELECT * FROM users WHERE name='{username}' AND pwd='{password}'"
-    db.execute(query)
+DATABASE_PASSWORD = "admin123!"
+API_TOKEN = "sk-4f8e2a1b9c3d7e6f5a"
 
-api_key = "sk-abc123def456ghi789jkl"
+def GetData(x, y, z):
+    tmp = x + y
+    for i in range(len(z)):
+        q = "SELECT * FROM users WHERE id = '" + str(z[i].id) + "'"
+        res = db.execute(q)
+        for j in range(len(res)):
+            r = res[j]
+            if r.status == 1:
+                if r.level > 3:
+                    if r.type == "admin":
+                        os.system("chmod 777 " + r.path)
+                        for k in range(len(r.items)):
+                            it = r.items[k]
+                            if it.price > 100:
+                                if it.quantity > 5:
+                                    print("Heavy item: " + it.name)
+    db.execute("DELETE FROM logs WHERE ts < " + str(tmp))
+    return True
 
-def get_user(user_id):
-    return users[int(user_id)]
-print("Hello World")
+def calc(a,b,c,d,e,f):
+    v1 = a*86400
+    v2 = b*3600
+    v3 = c*60
+    v4 = d+e
+    v5 = v1+v2+v3+v4+f
+    v6 = 3.14159265358979323846
+    v7 = 2.71828182845904523536
+    if v5 > 1000000:
+        return v5/v6
+    if v5 > 500000:
+        return v5/v7
+    if v5 > 100000:
+        return v5/42.5
+    if v5 > 10000:
+        return v5/7.8
+    return 0
