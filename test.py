efcd522f6e35ff6778826c6c2e9441b79a579cfc
ke230208qiu <1225873759@qq.com>
2026-08-03 17:27:42 +0800

    Refactor test.py by adding compute and long_function
    Removed login and get_user functions, added compute and long_function.

--- test.py
+++ test.py
@@ -1,11 +1,12 @@
 import os
+import sys
 
-def login(username, password):
-    query = f"SELECT * FROM users WHERE name='{username}' AND pwd='{password}'"
-    db.execute(query)
+def compute(a, b, c):
+    unused_var = 100
+    total = a + b + c
+    result = total * 2
+    print("result:", result)
 
-api_key = "sk-abc123def456ghi789jkl"
-
-def get_user(user_id):
-    return users[int(user_id)]
-print("Hello World")
+def long_function():
+    this_is_a_very_long_line = 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10 + 11 + 12 + 13 + 14 + 15 + 16 + 17 + 18 + 19 + 20 + 21 + 22 + 23 + 24 + 25 + 26
+    print(this_is_a_very_long_line)
